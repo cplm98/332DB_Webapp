30db0f2eb25d9586db76d59394fb84d99c79cd33
cplm98 <32113435+cplm98@users.noreply.github.com>
2019-03-20 12:11:27 -0400

    Got first student form inserting

--- templates/Edit.php
+++ templates/Edit.php
@@ -52,7 +52,52 @@
 
     <div class="home_title">
       <h1>Edit</h1>
+      <form method="post">
+        <div class="form-group">
+          <label for="sel1">Please select a type of attendee:</label>
+          <select name ="edit_select" class="form-control w-75" id="sel1">
+            <option>Add Student</option>
+            <option>Add Sponsored Attendee</option>
+            <option>Add Professional Attendee</option>
+            <option value="add_Sp">Add Sponsoring Company</option>
+            <option value="del_Sp">Delete Sponsoring Company</option>
+            <option value="edit_ses">Edit Session</option>
+          </select>
+        </div>
+        <input class="btn-light btn" type="submit" name ="send"/>
+      </form>
     </div>
+    <?php if (isset($_POST['send']) && $_POST['edit_select'] == "Add Student") : ?>
+      <form method="post">
+        <div class="form-group w-50">
+          <label for="email">Email address</label>
+          <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+          <label for="fname">First Name</lable>
+          <input class="form-control" id="fname" name="fname" placeholder="First Name">
+          <label for="lname">Last Name</lable>
+          <input class="form-control" id="lname" name="lname" placeholder="First Name">
+          <!-- Might wanna generate this behind the scenes -->
+          <label for="reg_num">Registration Number</lable>
+          <input class="form-control" id="reg_num" name="reg_num" placeholder="Registration Number">
+          <label for="room_num">Room Number</lable>
+          <input class="form-control" id="room_num" name="room_num" placeholder="Room Number">
+        </div>
+        <input class="btn-light btn" type="submit" name="student_sub"/>
+      </form>
+      <!-- maybe you can't have php between those two php if tags -->
+    <?php
+    echo "down here";
+    endif; ?>
+    <?php
+    if(isset($_POST['student_sub'])){
+      echo "here";
+      $dbh = new PDO('mysql:host=localhost;dbname=conferenceV2', "root", "");
+      $sql = "INSERT INTO attendees (registration_num, fname, lname, email, rate) VALUES (?,?,?,?,?)";
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute([$_POST['reg_num'], $_POST['fname'], $_POST['lname'], $_POST['email'], 50]);
+      $dbh = null;
+    }
+    ?>
 
   </div>
 
