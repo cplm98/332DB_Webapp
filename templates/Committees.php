e6ca28e9a80ace0504a83720ca1fd4057bd83722
Ryan Kinsella <15rklk@queensu.ca>
2019-03-21 13:40:12 -0400

    Committees completed

--- templates/Committees.php
+++ templates/Committees.php
@@ -14,6 +14,7 @@
   <!-- Bootstrap core CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="..\static\home_style.css">
+  <link rel="stylesheet" type="text/css" href="..\static\Committees.css">
 </head>
 
 <body>
@@ -53,6 +54,61 @@
     <div class="home_title">
       <h1>Committees</h1>
     </div>
+
+    <?php  //try to connect to database
+    try{
+    $pdo = new PDO('mysql:host=localhost;dbname=conferenceV2', "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+      echo "Connection failed: " . $e->getMessage();
+    }
+    ?>
+
+    <form method="post">
+      <div class="form-group">
+        <label for="sel1">Please select the sub-committee group:</label>
+        <select name ="sub-committee" class="form-control w-75" id="sel1">
+          <?php //fill the form with sub-committees
+              $rows = $pdo->query("SELECT comm_name FROM sub_committees");
+              if (empty($rows)){
+                echo "<option>No sub-committees availiable.</option>";
+              }
+              else{
+                while($row = $rows->fetch()){
+                  echo $row;
+                  if ($row['comm_name'] != NULL){
+                    echo "<option>", $row['comm_name'], "</option>";
+                  }
+                }
+              }
+          ?>
+        </select>
+      </div>
+      <input class="btn-light btn" type="submit" name ="send"/>
+    </form>
+
+    <?php //once submitted, list the first and last names from query
+      if(isset($_POST['send'])){
+        $selected_subcommittee = $_POST['sub-committee'];
+        echo "<p>$selected_subcommittee members:</p>";
+
+        $sql = "select fname, lname
+        from Sub_committees, Member_of, Attendees
+        where registration_num = member_id and comm_name = '$selected_subcommittee' and '$selected_subcommittee' = commitee_name";
+        $statement = $pdo->prepare($sql);
+        $statement->execute([$sql]);
+
+        echo "<table><tr><th>First Name</th><th>Last Name</th></tr>";
+        while ($row = $statement->fetch()) {
+        	echo "<tr><td>".$row["fname"]."</td><td>".$row["lname"]."</td></tr>";
+        }
+        $pdo = null;
+      }
+    ?>
+
+
 
   </div>
 
