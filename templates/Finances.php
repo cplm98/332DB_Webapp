887b3b296d034e555be6256d154f01b2ed6ff010
Ryan Kinsella <15rklk@queensu.ca>
2019-03-22 11:01:00 -0400

    Finances finished besides proper formatting, tables out of line

--- templates/Finances.php
+++ templates/Finances.php
@@ -68,51 +68,64 @@
 
   <div class="registered_attendees">
     <h2>Registered Attendees</h2>
+    <?php
+
+    echo "<table class='table w-75 table-light table-bordered' border=1 align=left>", "<tr><th>", "","</th>","<th>", "Students","</th>","<th>","Professionals", "</th><th>", "Sponsors" ,"</th></tr>";
+    echo "<tr><th>", "Cost ($)","</th>","<th>", "50","</th>","<th>","100", "</th><th>", "0" ,"</th></tr>";
+    echo "<tr><th>", "Number Attending","</th>";
+
+    $sqlStudents = "select count(registration_num) as numStudents from students";
+    $sqlProfessionals ="select count(registration_num) as numProfessionals from professional";
+    $sqlSponsors = "select count(registration_num) as numSponsors from sponsors";
+
+    $studentStatement = $pdo->prepare($sqlStudents);
+    $studentStatement->execute([$sqlStudents]);
+    $studentStatement = $studentStatement->fetch()["numStudents"];
+    echo "<th>".$studentStatement."</th>";
+
+    $professionalsStatement = $pdo->prepare($sqlProfessionals);
+    $professionalsStatement->execute([$sqlProfessionals]);
+    $professionalsStatement=$professionalsStatement->fetch()["numProfessionals"];
+    echo "<th>".$professionalsStatement."</th>";
+
+    $sponsorsStatement = $pdo->prepare($sqlSponsors);
+    $sponsorsStatement->execute([$sqlSponsors]);
+    echo "<th>".$sponsorsStatement->fetch()["numSponsors"]."</th></tr>";
+
+    echo "<tr><th>", "Net Gain ($)","</th>";
+    $studentsEarned = $studentStatement * 50;
+    echo "<th>",$studentsEarned,"</th>";
+    $professionalsEarned = $professionalsStatement * 50;
+    echo "<th>",$professionalsEarned,"</th>";
+    echo "<th>","0","</th></tr>";
+
+     ?>
+    </div>
+
+
+
+     <br>
+     <div class="Sponsorships">
+       <h2>Sponsorships</h2>
+
+       <?php
+       $totalSponsor = 0;
+       $rows = $pdo->query("select comp_name, comp_level, spons_rate from company");
+       echo "<table class='table w-75 table-light table-bordered' border=1 align=left>", "<tr><th>", "Company Name","</th>","<th>","Level", "</th><th>", "Sponsorship Rate" ,"</th></tr>";
+       while ($row = $rows->fetch()) {
+         echo "<table", "<tr><td>" , $row['comp_name'], "</td><td>", $row['comp_level'] ,"</td><td>",$row['spons_rate'] ,"</td></tr>";
+         $totalSponsor = $row['spons_rate'] + $totalSponsor;
+       }
+
+       echo "<h3>Total earned through sponsorships: $ $totalSponsor</h3>";
+       ?>
+
+   </div>
   </div>
+   <?php
 
-  <?php
-  echo "<table class='table w-75 table-light table-bordered' border=1 align=left>", "<tr><th>", "","</th>","<th>", "Students","</th>","<th>","Professionals", "</th><th>", "Sponsors" ,"</th></tr>";
-  echo "<tr><th>", "Cost ($)","</th>","<th>", "50","</th>","<th>","100", "</th><th>", "0" ,"</th></tr>";
-  echo "<tr><th>", "Number Attending","</th>";
-
-  $sqlStudents = "select count(registration_num) as numStudents from students";
-  $sqlProfessionals ="select count(registration_num) as numProfessionals from professional";
-  $sqlSponsors = "select count(registration_num) as numSponsors from sponsors";
-
-  $studentStatement = $pdo->prepare($sqlStudents);
-  $studentStatement->execute([$sqlStudents]);
-  $studentStatement = $studentStatement->fetch()["numStudents"];
-  echo "<th>".$studentStatement."</th>";
-
-  $professionalsStatement = $pdo->prepare($sqlProfessionals);
-  $professionalsStatement->execute([$sqlProfessionals]);
-  $professionalsStatement=$professionalsStatement->fetch()["numProfessionals"];
-  echo "<th>".$professionalsStatement."</th>";
-
-  $sponsorsStatement = $pdo->prepare($sqlSponsors);
-  $sponsorsStatement->execute([$sqlSponsors]);
-  echo "<th>".$sponsorsStatement->fetch()["numSponsors"]."</th></tr>";
-
-  echo "<tr><th>", "Net Gain ($)","</th>";
-  $studentsEarned = $studentStatement * 50;
-  echo "<th>",$studentsEarned,"</th>";
-  $professionalsEarned = $professionalsStatement * 50;
-  echo "<th>",$professionalsEarned,"</th>";
-  echo "<th>","0","</th></tr>";
-
-  //
-  // while ($row = $statement->fetch()) {
-  //   echo "<th>".$row["numStudents"]."</th><th>".$row["numProfessional"]."</th><th>".$row["numSponsors"]."</th></tr>";
-  // }
-
-
-  $pdo = null;
-
-
-   ?>
-
-
-  </div>
+   $pdo = null;
+    ?>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
